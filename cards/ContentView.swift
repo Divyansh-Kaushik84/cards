724efe3c39e8981f4511678fd3603c2aae9d4b66
Divyansh-Kaushik84 <84divyanshkaushik@gmail.com>
2024-08-25 12:32:53 +0530

    Initial Commit

---
+++ cards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cards
+//
+//  Created by Divyansh Kaushik on 25/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cards/cardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardsApp.swift
+//  cards
+//
+//  Created by Divyansh Kaushik on 25/08/24.
+//
+
+import SwiftUI
+
+@main
+struct cardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
